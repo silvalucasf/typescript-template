287334b286842fe6c3f56bb890e4c3dd1a171e43
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2024-07-16 12:15:47 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+function test() {
+  console.log();
+}
+
+const test = 2;
+
 console.log('Hello');
 console.log('Hello');
 console.log('Hello');
 console.log('Hello');
 console.log('Hello');
 console.log('Hello');
-casd;
